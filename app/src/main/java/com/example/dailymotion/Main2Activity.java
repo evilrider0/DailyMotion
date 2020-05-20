9131d1a647e90a0db07297321eb7d110386c77ea
Manishankar Vakta <evilrider555@gmail.com>
2020-05-20 11:10:41 +0600

    Initial commit

---
+++ app/src/main/java/com/example/dailymotion/Main2Activity.java
@@ -0,0 +1,20 @@
+package com.example.dailymotion;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+    }
+
+}
